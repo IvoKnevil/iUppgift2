811ff27a53e3cc8aad305e6c9c04fd5f4640052b
Ivo Nazlic <78ivonaz@gafe.molndal.se>
2020-12-01 22:41:59 +0100

    Add project files.

---
+++ iUppgift2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace iUppgift2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+
+        static void Welcome ()
+        {
+            
+
+            Console.WriteLine("Välkommen till superprogrammet.");
+            Console.Write("Vänligen ange din inloggningskod: ");
+            string userInputCode = Console.ReadLine();
+
+
+        }
+
+    }
+}
